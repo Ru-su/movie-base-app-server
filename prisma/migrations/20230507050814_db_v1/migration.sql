c684ace6eef67f8ba8677a8b17430b79faf76267
Rustem Shakirov <rustem_shakirov@yahoo.com>
2023-05-07 14:14:51 +0900

    add Prisma schema

---
+++ prisma/migrations/20230507050814_db_v1/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "verificationStatus" BOOLEAN NOT NULL,
+    "hash" TEXT NOT NULL,
+    "fisrstName" TEXT,
+    "lastName" TEXT,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tokens" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "token" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Favorites" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "favorite" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Favorites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Movies" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "movieId" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Movies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tokens_token_key" ON "Tokens"("token");
+
+-- AddForeignKey
+ALTER TABLE "Favorites" ADD CONSTRAINT "Favorites_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Movies" ADD CONSTRAINT "Movies_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Favorites"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
